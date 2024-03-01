8ac116463cbd13ac14c7291436861d5e2c5f8c6d
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-01 14:36:13 -0800

    Finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversedArray = splitString.reverse();
+    let reversedString = reversedArray.join("");
 
+    return reversedString;;
 };
 
 // Do not edit below this line
