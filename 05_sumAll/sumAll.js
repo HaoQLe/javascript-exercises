365a6062cd18f24a3226c72b0017b31fa686bc4f
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-02 23:34:05 -0800

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    finalSum = 0
 
+    if (start < 0 || stop < 0) {
+        return "ERROR";
+    }
+
+    if (typeof(start) != "number" || typeof(stop) != "number") {
+        return "ERROR";
+    }
+
+    if (start < stop) {
+        for (let i = start; i <= stop; i++) {
+            finalSum += i;
+        }
+    } else {
+        for (let i = start; i >= stop; i--) {
+            finalSum += i;
+        }
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
