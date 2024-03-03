4d80d5bbdcdbad0c4976fa895d53b1d88d4bf2bc
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-02 23:58:57 -0800

    Finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const tempC = (tempF - 32) * 5/9;
+  const rounded = Math.round(tempC * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const tempF = (tempC * (9/5)) + 32;
+  const rounded = Math.round(tempF * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
