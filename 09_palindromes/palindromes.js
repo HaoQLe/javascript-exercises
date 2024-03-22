283c64ed317efbe3aad2997f432637e227cabbc0
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-21 19:28:32 -0700

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str
+            .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')  // strip all punctuation
+            .toLowerCase()  // lowercase string
+            .replace(/\s/g, '');
 
+
+    let left = 0;
+    let right = str.length - 1
+
+    while (left <= right) {
+        if (str.charAt(left) != str.charAt(right)) {
+            return false;
+        }
+
+        left += 1;
+        right -= 1;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
