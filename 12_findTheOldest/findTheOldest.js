65d3f0bb18a54b7840551ca71a1bc534fd49ebb4
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-21 19:56:56 -0700

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, currPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currAge = getAge(currPerson.yearOfBirth, currPerson.yearOfDeath);
 
+        if (currAge > oldestAge) {
+            return currPerson;
+        } else {
+            return oldest;
+        }
+    });
 };
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
