d293c7343d50f1f8d553585938a4d5a76a0f948f
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-21 19:39:48 -0700

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n == 1) {
+        return 1;
+    }
+
+    let prevTermOne = 0;
+    let prevTermTwo = 1;
+    let currentTerm = 0;
+
+    for (let i = 2; i <= n; i++) {
+        currentTerm = prevTermOne + prevTermTwo;
+        prevTermOne = prevTermTwo;
+        prevTermTwo = currentTerm;
+    }
+
+    return currentTerm;
 };
 
 // Do not edit below this line
